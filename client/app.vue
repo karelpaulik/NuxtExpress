c5ff52f217078979ea03cd162b31bb7be2d4addc
karel.paulik <karelpaulik@email.cz>
2024-09-20 13:28:46 +0200

    client/app.vue edited. Design improvements.

--- client/app.vue
+++ client/app.vue
@@ -1,229 +1,179 @@
-<template>  
+<template>
   <q-layout view="hHh LpR fff">
-
-    <q-header elevated class="bg-primary text-white" height-hint="98">
+    <!-- Header -->
+    <q-header elevated class="bg-grey-5 text-black" height-hint="98">
       <q-toolbar align="center">
         <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
         <q-toolbar-title>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+          <q-avatar size="64px">
+            <img src="tygr.jpg" />
           </q-avatar>
-          Title
+          Web page title
         </q-toolbar-title>
-        <pre>logedUser: {{ logedUser.email }}</pre>
+        <q-tabs align="center" no-caps>
+          <q-route-tab v-if="!logedUser.email" to="/userAuth/login" label="Login" />
+          <q-route-tab v-if="logedUser.email" to="/userAuth/logout" label="Logout" />
+        </q-tabs>
+        <!-- <pre>logedUser: {{ logedUser.email }}</pre> -->
+        <span>LogedUser:</span>&nbsp;&nbsp;<span>{{ logedUser.email }}</span> <!-- &nbsp;&nbsp; - 2x white space -->
         <q-btn dense flat round icon="menu" @click="toggleRightDrawer" />
       </q-toolbar>
-      <q-tabs align="center" no-caps="">
-        <q-route-tab to="/" label="Index" />
-        <q-route-tab to="/private" label="Private" />
-        <q-route-tab to="/userAuth/login" label="Login" />
-        <q-route-tab to="/userAuth/loginInfo" label="Login info" />
-        <q-route-tab to="/userAuth/logout" label="Logout" />
-        <q-route-tab to="/signup" label="Signup" />
+      
+      <!-- Tabs -->
+      <q-tabs align="center" no-caps>
+        <q-route-tab v-for="tab in tabs" :key="tab.label" :to="tab.to" :label="tab.label" />
         <a :href="`${runtimeConfig.public.baseURL}/expresslogin`">expressjs login - zadní vrátka</a>
+        
+        <!-- Player Dropdown -->
         <q-btn-dropdown no-caps auto-close stretch flat label="Player">
           <q-list separator>
-            <q-item>
-              <q-route-tab to="/player/" label="Index" />
-            </q-item>
-            <q-item>
-              <q-route-tab to="/player/playerList" label="PlayerList, Privat - client and server" />
-            </q-item>
-            <q-item>
-              <q-route-tab to="/player/player-" label="Player" />
+            <q-item v-for="player in playerLinks" :key="player.label">
+              <q-route-tab :to="player.to" :label="player.label" />
             </q-item>
           </q-list>
         </q-btn-dropdown>
       </q-tabs>
     </q-header>
 
-    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered class="bg-grey-2">
+    <!-- Left Drawer -->
+    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered class="bg-grey-3">
       <q-list bordered separator>
-        <q-item clickable v-ripple><NuxtLink to="/">Home page</NuxtLink></q-item>
-        <q-item clickable v-ripple><q-icon name="phone_iphone" />Item</q-item>
-        <q-item clickable v-ripple><q-icon name="thumb_up" size="md" color="pink-3" />Item1</q-item>
-        <q-item clickable v-ripple><q-icon name="o_thumb_up" size="md" color="teal-14" />Item2</q-item>
+        <q-item v-for="item in leftDrawerItems" :key="item.label" clickable v-ripple>
+          <q-icon :name="item.icon" :color="item.color" />
+          <NuxtLink :to="item.link">{{ item.label }}</NuxtLink>
+        </q-item>
       </q-list>
     </q-drawer>
 
-    <q-drawer v-model="rightDrawerOpen" side="right" bordered class="bg-grey-2">
+    <!-- Right Drawer -->
+    <q-drawer v-model="rightDrawerOpen" side="right" bordered class="bg-grey-3">
       <q-list>
         <q-item-label header>Essential Links</q-item-label>
-        <q-item clickable target="_blank" rel="noopener" href="https://quasar.dev">
+        <q-item v-for="link in essentialLinks" :key="link.label" clickable target="_blank" rel="noopener" :href="link.url">
           <q-item-section avatar>
-            <q-icon name="school" />
+            <q-icon :name="link.icon" />
           </q-item-section>
           <q-item-section>
-            <q-item-label>Docs</q-item-label>
-            <q-item-label caption>asdf asdf</q-item-label>
-            <q-item-label caption>asdf asdf</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item clickable target="_blank" rel="noopener" href="https://github.quasar.dev">
-          <q-item-section avatar>
-            <q-icon name="code" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>GitHub</q-item-label>
-            <q-item-label caption>github.com/quasarframework</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item clickable target="_blank" rel="noopener" href="http://chat.quasar.dev">
-          <q-item-section avatar>
-            <q-icon name="chat" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Discord Chat Channel</q-item-label>
-            <q-item-label caption>https://chat.quasar.dev</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item clickable target="_blank" rel="noopener" href="https://forum.quasar.dev">
-          <q-item-section avatar>
-            <q-icon name="record_voice_over" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Forum</q-item-label>
-            <q-item-label caption>https://forum.quasar.dev</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item clickable target="_blank" rel="noopener" href="https://twitter.quasar.dev">
-          <q-item-section avatar>
-            <q-icon name="rss_feed" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Twitter</q-item-label>
-            <q-item-label caption>@quasarframework</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item clickable target="_blank" rel="noopener" href="https://facebook.quasar.dev">
-          <q-item-section avatar>
-            <q-icon name="public" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Facebook</q-item-label>
-            <q-item-label caption>@QuasarFramework</q-item-label>
+            <q-item-label>{{ link.label }}</q-item-label>
+            <q-item-label caption>{{ link.caption }}</q-item-label>
           </q-item-section>
         </q-item>
       </q-list>
 
+      <!-- Additional Content -->
       <q-list bordered class="rounded-borders" style="max-width: 350px">
         <q-item-label header>Friends</q-item-label>
-
-        <q-item clickable v-ripple>
+        <q-item v-for="friend in friends" :key="friend.name" clickable v-ripple>
           <q-item-section avatar>
-            <q-avatar>
-              <img src="https://cdn.quasar.dev/img/avatar2.jpg">
-            </q-avatar>
+            <q-avatar><img :src="friend.avatar" /></q-avatar>
           </q-item-section>
-
           <q-item-section>
-            <q-item-label lines="1">Brunch this weekend?</q-item-label>
+            <q-item-label lines="1">{{ friend.title }}</q-item-label>
             <q-item-label caption lines="2">
-              <span class="text-weight-bold">Janet</span>
-              -- I'll be in your neighborhood doing errands this
-              weekend. Do you want to grab brunch?
+              <span class="text-weight-bold">{{ friend.name }}</span> -- {{ friend.message }}
             </q-item-label>
           </q-item-section>
-
-          <q-item-section side top>
-            1 min ago
-          </q-item-section>
-        </q-item>
-
-        <q-separator inset="item" />
-
-        <q-item clickable v-ripple>
-          <q-item-section avatar>
-            <q-avatar>
-              <img src="https://cdn.quasar.dev/img/avatar4.jpg">
-            </q-avatar>
-          </q-item-section>
-
-          <q-item-section>
-            <q-item-label lines="1">Linear Project</q-item-label>
-            <q-item-label caption lines="2">
-              <span class="text-weight-bold">John</span>
-              -- Can we schedule a call for tomorrow?
-            </q-item-label>
-          </q-item-section>
-
-          <q-item-section side top>
-            1 min ago
-          </q-item-section>
+          <q-item-section side top>{{ friend.time }}</q-item-section>
         </q-item>
       </q-list>
 
+      <!-- Uploader -->
       <div class="text-h4 text-weight-bold">Nadpis</div>
-
-      <q-uploader
-      url="http://localhost:4444/upload"
-      style="max-width: 300px"
-      />
+      <q-uploader url="http://localhost:4444/upload" style="max-width: 300px" />
     </q-drawer>
 
+    <!-- Page Content -->
     <q-page-container>
-      <q-page class="bg-teal-2">
+      <q-page class="bg-grey-6">
         <NuxtPage />
         <!-- <router-view /> -->
       </q-page>
     </q-page-container>
 
+    <!-- Footer -->
     <q-footer elevated class="bg-grey-8 text-white">
       <q-toolbar>
         <q-toolbar-title>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
-          </q-avatar>
+          <q-avatar><img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" /></q-avatar>
           <div>Title</div>
         </q-toolbar-title>
       </q-toolbar>
     </q-footer>
-
   </q-layout>
 </template>
 
 <script setup>
-  import { ref } from 'vue'
+import { ref } from 'vue';
 
-  //Vypsání použité sady ikon (dá se měnit v "nuxt.config.ts")
-  // import { useQuasar } from 'quasar'
-  // const $q = useQuasar()
-  // console.log($q.iconSet);
+//Vypsání použité sady ikon (dá se měnit v "nuxt.config.ts")
+// import { useQuasar } from 'quasar'
+// const $q = useQuasar()
+// console.log($q.iconSet);
 
-  const leftDrawerOpen = ref(false)
-  const rightDrawerOpen = ref(false)
+// Drawer states
+const leftDrawerOpen = ref(false);
+const rightDrawerOpen = ref(false);
 
-  function toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-  }
+// Functions to toggle drawers
+const toggleLeftDrawer = () => leftDrawerOpen.value = !leftDrawerOpen.value;
+const toggleRightDrawer = () => rightDrawerOpen.value = !rightDrawerOpen.value;
+//Old solution was
+// function toggleLeftDrawer () {
+//   leftDrawerOpen.value = !leftDrawerOpen.value
+// }
 
-  function toggleRightDrawer () {
-        rightDrawerOpen.value = !rightDrawerOpen.value
-  }
+// Tabs and Links
+const tabs = [
+  { label: 'Index', to: '/' },
+  { label: 'Private', to: '/private' },
+  { label: 'Login info', to: '/userAuth/loginInfo' },
+  { label: 'Signup', to: '/signup' }
+];
 
-  const runtimeConfig = useRuntimeConfig();
+const playerLinks = [
+  { label: 'Index', to: '/player/' },
+  { label: 'PlayerList', to: '/player/playerList' },
+  { label: 'Player', to: '/player/player-' }
+];
+
+const leftDrawerItems = [
+  { label: 'Home page', link: '/', icon: 'home' },
+  { label: 'Item', link: '', icon: 'phone_iphone' },
+  { label: 'Item1', link: '', icon: 'thumb_up', color: 'pink-3' },
+  { label: 'Item2', link: '', icon: 'o_thumb_up', color: 'teal-14' }
+];
+
+const essentialLinks = [
+  { label: 'Docs', icon: 'school', caption: 'Quasar docs', url: 'https://quasar.dev' },
+  { label: 'GitHub', icon: 'code', caption: 'github.com/quasarframework', url: 'https://github.quasar.dev' },
+  { label: 'Discord', icon: 'chat', caption: 'chat.quasar.dev', url: 'http://chat.quasar.dev' }
+];
+
+// Friend data
+const friends = [
+  { name: 'Janet', avatar: 'https://cdn.quasar.dev/img/avatar2.jpg', title: 'Brunch this weekend?', message: 'I\'ll be in your neighborhood...', time: '1 min ago' },
+  { name: 'John', avatar: 'https://cdn.quasar.dev/img/avatar4.jpg', title: 'Linear Project', message: 'Can we schedule a call for tomorrow?', time: '1 min ago' }
+];
+
+// Runtime config and logged user info
+const runtimeConfig = useRuntimeConfig();
   //const logedUser = useState('logedUser', () => ({email: ''}));
   //const logedUser = useLocalStorage('u-storage', {email: ''})
-  const logedUser = useLogedUser();
+const logedUser = useLogedUser();
 
   //Toto je, protože localStorage se po zavření/otevření nesmaže.
   //Kdyby se podařilo localStorage se zavřením prohlížeče smazat (ale nemazat se zavřením záložky), tak řádky níže nejsou potřeba.
   if (process.client) { //provádím pouze na client side.
     const { data, refresh } = await useFetch('/api/auth', {
-        method: 'get',
-        baseURL: runtimeConfig.public.baseURL,
-        headers: useRequestHeaders(['cookie']),
-        credentials: 'include',
-        onResponse({response}) {
-          if (response.status == 200) {
-            logedUser.value.email = response._data.email;
-          }
-          if (response.status == 401) {
-            logedUser.value.email = "";
-          }
-        }
-    });
-  }
+    method: 'get',
+    baseURL: runtimeConfig.public.baseURL,
+    headers: useRequestHeaders(['cookie']),
+    credentials: 'include',
+    onResponse({ response }) {
+      logedUser.value.email = response.status === 200 ? response._data.email : '';
+    }
+  });
+}
 
 // //Pozastavení zavžení záložky nebo prohlížeče
 // if (process.client) {
@@ -234,7 +184,7 @@
 // }
 </script>
 
-<style>
+<style scoped>
 body {
   background-color: gray;
 }
@@ -244,5 +194,4 @@ body {
   height: 230px;
   max-width: 250px;
 }
-
 </style>
